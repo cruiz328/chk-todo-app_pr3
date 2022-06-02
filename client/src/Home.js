20ba7eda2d13be5eb9b859a9d5c934a1127d4501
cruiz328 <lemmy125@gmail.com>
2022-06-01 20:54:34 -0700

    added todo UI buttons and comment sections

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,4 +1,4 @@
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Nav, Navbar, Container, Button, Row, Col, FloatingLabel, Form } from 'react-bootstrap';
 
 export const Home = () => {
     return (
@@ -9,10 +9,40 @@ export const Home = () => {
     <Navbar.Brand href="#home">C.H.K. Todo App</Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="/home">Home</Nav.Link>
-      <Nav.Link href="/users">users</Nav.Link>
+      <Nav.Link href="/users">Login</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
+
+  <Container>
+
+<Col xs={12} md={12} lg={12}>
+
+</Col>
+
+    <Row xs={12} md={12} lg={12}>
+        <Col xs={12} md={12} lg={12}>
+
+        <Button variant="secondary">new todo</Button>
+
+        <>
+  <FloatingLabel controlId="floatingTextarea" label="Title" className="mb-3">
+    <Form.Control as="textarea" placeholder="Leave a comment here" />
+  </FloatingLabel>
+  <FloatingLabel controlId="floatingTextarea2" label="Todo Notes">
+    <Form.Control
+      as="textarea"
+      placeholder="Leave a comment here"
+      style={{ height: '100px' }}
+    />
+  </FloatingLabel>
+  <Button variant="success">add todo</Button>
+</>
+
+        </Col>
+    </Row>
+
+  </Container>
    </>
     )
 }
