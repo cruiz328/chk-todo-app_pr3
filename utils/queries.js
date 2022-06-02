1fa41dd69211651fd4579aef40c5b35a3fcfee1e
HamedSay <88417183+HamedSay@users.noreply.github.com>
2022-06-01 17:19:28 -0700

    added graphql

---
+++ utils/queries.js
@@ -0,0 +1,26 @@
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
