b4d8d31b6af5cedc847a2e0aff97347fe3042b62
keneversley <krazorfbx@gmail.com>
2022-06-03 15:17:21 -0400

    test1

--- client/src/mutations/todoMutations.js
+++ client/src/mutations/todoMutations.js
@@ -1,5 +1,6 @@
-import { gql } from '@apollo/client';
 
+   
+import { gql } from '@apollo/client';
 
 export const ADD_TODO = gql`
     mutation CreateUser($firstName: String!, $lastName: String!) {
